27dd64a5cfa89a7d7b0724bacf1da91b8d0df3b4
Pawel Tiutiurski <pawel.tiutiurski@ifresearch.org>
2020-03-25 21:51:00 +0100

    small changes

--- src/constants.py
+++ src/constants.py
@@ -1,9 +1,9 @@
 import os
 from datetime import datetime
-from config import TRICITY_DATA
+from config import TRICITY_STATE
 
 ORIGINS_COORD = '54.38, 18.55937'
 
-STOPS_3CITY = os.path.join(TRICITY_DATA, 'stops_3city_checked.csv')
+STOPS_3CITY = os.path.join(TRICITY_STATE, 'stops_3city_checked.csv')
 
 time = datetime.now()
