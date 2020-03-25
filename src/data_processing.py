d7cd48ddddc15030105c49d163584d6b8f1a32fe
Pawel Tiutiurski <pawel.tiutiurski@ifresearch.org>
2020-03-25 19:50:34 +0100

    fix small mistake, add debugs, add descriptions

--- src/data_processing.py
+++ src/data_processing.py
@@ -5,7 +5,11 @@ from config import TRICITY, logger
 from distance_matrix_api import get_time_of_travel
 
 
-def split_df(data, start, stop):
+def split_df(data: pd.DataFrame, start: int, stop: int) -> pd.DataFrame:
+    """
+    return part of dataframe.
+    google api allows on 100 coordinates in one request
+    """
     df = data.iloc[start:stop, :].copy()
     df['coord'] = df.apply(lambda x: f"{x['stopLat']}, {x['stopLon']}", axis=1)
     return df.sort_values('coord')
@@ -25,26 +29,43 @@ def get_zone(result):
     return zones
 
 
-def prepare_stops_to_request(df):
+def prepare_stops_to_request(df: pd.DataFrame) -> list:
+    """
+    return list of splited dataframes
+    """
     return [split_df(df, i, i + 100) for i in range(0, len(df), 100)]
 
 
-def google_api_request(dfs_splited):
+def google_api_request(dfs_splited: list) -> list:
+    """
+    requests to google api in loop by splited dataframes
+    """
     return [get_time_of_travel(
         ORIGINS_COORD, desttination['coord'].to_list(), time)
         for desttination in dfs_splited]
 
 
-def add_times_of_travels(dfs_splited):
+def add_times_of_travels(dfs_splited: list) -> list:
+    """
+    extract time from google api answers
+    add results to splited dataframes
+    """
     results = google_api_request(dfs_splited)
+    logger.debug("Ready answer for request")
+
     travel_times = [[i['duration']['value']
                      for i in result['rows'][0]['elements']]
                     for result in results]
+    logger.debug("Times of travel extracted")
+
     return [df.assign(time_sec=time_t)
             for df, time_t in zip(dfs_splited, travel_times)]
 
 
 def save_times_data():
+    """
+    save data from requests to csv file
+    """
     df = pd.read_csv(STOPS_3CITY, index_col=0)
     logger.debug("Data readed")
 
@@ -56,11 +77,12 @@ def save_times_data():
 
     df_merged = pd.DataFrame()
     for data_part in dfs_with_times:
-        df_merged = df_merged.append(df)
+        df_merged = df_merged.append(data_part)
     logger.debug("Dataframes prepared to save")
 
     date = time.strftime('%m-%d-%Y_%H-%M-%S')
-    df_merged.to_csv(os.path.join(TRICITY, "data", f"tricity_travel_{date}"))
+    df_merged.to_csv(os.path.join(TRICITY, "data",
+                                  f"tricity_travel_{date}.csv"))
     logger.debug("Data saved")
 
 
