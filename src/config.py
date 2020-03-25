37ef6880f89f50f6e33ddd3a9dc3e53e2d0074ff
Pawel Tiutiurski <pawel.tiutiurski@ifresearch.org>
2020-03-25 19:49:32 +0100

    logger

--- src/config.py
+++ src/config.py
@@ -4,31 +4,11 @@ import logging
 TRICITY = os.environ.get('tricity', '~/3city-transport/')
 TRICITY_DATA = os.environ.get('tricity_data', '~/3city-transport/src/state')
 
-logger = logging.getLogger(__file__)
+logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
-
-def configure_logger(filepath: str = None, loglevel=logging.DEBUG):
-    logger.setLevel(logging.DEBUG)
-
-    log_format = logging.Formatter(
-        '[%(asctime)s] [%(levelname)s] %(message)s'
-        #    '[--> %(pathname)s [%(process)d]:]')
-    )
-
-    consoleHandler = logging.StreamHandler()
-    consoleHandler.setLevel(loglevel)
-    consoleHandler.setFormatter(log_format)
-
-    logger.addHandler(consoleHandler)
-
-    # file logger only on demand
-
-    if filepath:
-        fileHandler = logging.FileHandler(filepath)
-        fileHandler.setLevel(logging.DEBUG)
-        fileHandler.setFormatter(log_format)
-
-        logger.addHandler(fileHandler)
-
-    return logger
+handler = logging.StreamHandler()
+handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
