f9b80280c177a10755581bbdf824753e57e44c88
Pawel Tiutiurski <pawel.tiutiurski@ifresearch.org>
2020-03-25 21:51:09 +0100

    cleanup

--- src/data_processing.py
+++ src/data_processing.py
@@ -15,20 +15,6 @@ def split_df(data: pd.DataFrame, start: int, stop: int) -> pd.DataFrame:
     return df.sort_values('coord')
 
 
-def get_zone(result):
-    adresses = result['destination_addresses']
-    city = [place.split(', ')[1] for place in adresses]
-
-    zones = []
-    for z in city:
-        element = z.split(' ')
-        if len(element) > 1:
-            zones.append(element[1])
-        else:
-            zones.append(element[0])
-    return zones
-
-
 def prepare_stops_to_request(df: pd.DataFrame) -> list:
     """
     return list of splited dataframes
